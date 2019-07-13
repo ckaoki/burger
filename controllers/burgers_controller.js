efceab3a878ecdddedda287ca4d000ab7a3a3471
ckaoki <cullan_aoki@msn.com>
2019-07-12 18:15:06 -0700

    added gitignore. Made comment changes in a couple files.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -18,7 +18,6 @@ router.post('/api/burgers', function(req, res){
 
 router.put('/api/burgers/:id', function(req, res){
   var condition = 'id = ' + req.params.id;
-  // console.log("condition: ", condition);
   console.log("devoured: ");
   
   burger.updateBurger(
